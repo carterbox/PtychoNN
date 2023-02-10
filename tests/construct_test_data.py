32b5155c92dfb32e89daf3cd2982b436ac1d1ed4
Daniel Ching <carterbox@users.noreply.github.com>
2023-02-10 16:11:47 -0600

    TST: Use a more realisitic probe

--- tests/construct_test_data.py
+++ tests/construct_test_data.py
@@ -3,38 +3,64 @@ import os
 
 import libimage
 import numpy as np
-import tifffile
+import matplotlib.pyplot as plt
 import tike.ptycho
 import tike.ptycho.learn
 
 
 def test_construct_simulated_training_set(
-    W=2048,
-    N=1024,
-    S=128,
+    width=1024,
+    num_scan=1024,
+    S=64,
     out_dir='simulated',
 ):
 
     os.makedirs(out_dir, exist_ok=True)
 
-    probe = (tike.ptycho.probe.gaussian(S, 0.6, 0.9)[None, None, None, ...] *
-             np.exp(1j * np.pi *
-                    (np.random.rand(S, S) - 0.5))).astype('complex64')
+    shape = S
+    lambda0 = 1.24e-9 / 10
+    dx_dec = 75e-6
+    dis_defocus = 800e-6
+    dis_StoD = 2
+    dx = lambda0 * dis_StoD / shape / dx_dec
 
-    tifffile.imwrite(f'{out_dir}/probe-amplitude.tiff', np.abs(probe[0, 0, 0]))
-    tifffile.imwrite(f'{out_dir}/probe-phase.tiff', np.angle(probe[0, 0, 0]))
+    # test single probe modes
+    probe = tike.ptycho.fresnel.single_probe(shape,
+                                             lambda0,
+                                             dx,
+                                             dis_defocus,
+                                             zone_plate_params=dict(
+                                                 radius=150e-6 / 2,
+                                                 outmost=50e-9,
+                                                 beamstop=60e-6,
+                                             ))
 
-    for a, b in itertools.permutations(libimage.available_images, 2):
+    plt.imsave(f'{out_dir}/probe-amplitude.png', np.abs(probe[0, 0, 0]))
+    plt.imsave(f'{out_dir}/probe-phase.png',
+               np.angle(probe[0, 0, 0]),
+               vmin=-np.pi,
+               vmax=np.pi,
+               cmap=plt.cm.twilight)
 
-        phase = libimage.load(a, W) - 0.5
-        amplitude = 1 - libimage.load(b, W)
-        fov = (amplitude * np.exp(1j * phase * np.pi)).astype('complex64')
-        tifffile.imwrite(f'{out_dir}/phase-{a}.tiff', np.angle(fov))
-        tifffile.imwrite(f'{out_dir}/amplitude-{b}.tiff', np.abs(fov))
-        assert np.abs(fov).max() <= 1.0
-        assert np.abs(fov).min() >= 0.0
+    for a, b in itertools.permutations(libimage.available_images[:3], 2):
 
-        scan = np.random.uniform(1, W - S - 1, (N, 2))
+        phase = (libimage.load(a, width) - 0.5).astype(np.csingle)
+        amplitude = (1.0 - libimage.load(b, width)).astype(np.csingle)
+        fov = (amplitude * np.exp(1j * phase * np.pi))
+        plt.imsave(f'{out_dir}/phase-{a}.png',
+                   np.angle(fov),
+                   vmin=-np.pi,
+                   vmax=np.pi,
+                   cmap=plt.cm.twilight)
+        plt.imsave(f'{out_dir}/amplitude-{b}.png', np.abs(fov))
+        assert np.abs(fov).max(
+        ) <= 1.001, f'f{np.abs(fov).max()} is larger than 1.0 for {b}'
+        assert np.abs(fov).min(
+        ) >= 0.0, f'f{np.abs(fov).min()} is smaller than 0.0 for {b}'
+        assert np.angle(fov).max() <= +np.pi, a
+        assert np.angle(fov).min() >= -np.pi, a
+
+        scan = np.random.uniform(1, width - S - 1, (num_scan, 2))
 
         patches = tike.ptycho.learn.extract_patches(
             psi=fov,
@@ -53,8 +79,8 @@ def test_construct_simulated_training_set(
             axes=(-2, -1),
         ).astype('float32')
         print(diffraction.dtype, diffraction.shape)
-        tifffile.imwrite(f'{out_dir}/diffraction-{a}-{b}.tiff',
-                         diffraction[N // 2])
+        plt.imsave(f'{out_dir}/diffraction-{a}-{b}.png',
+                   diffraction[num_scan // 2])
 
         print(f'Training params = {np.prod(diffraction.shape)}')
 
---
+++ tests/construct_test_imagenet.py
@@ -0,0 +1,95 @@
+import itertools
+import os
+
+import libimage
+import numpy as np
+import matplotlib.pyplot as plt
+import tike.ptycho
+import tike.ptycho.learn
+
+
+def test_construct_simulated_training_set(
+    width=1024,
+    num_scan=1024,
+    S=64,
+    out_dir='imagenet',
+):
+
+    os.makedirs(out_dir, exist_ok=True)
+
+    shape = S
+    lambda0 = 1.24e-9 / 10
+    dx_dec = 75e-6
+    dis_defocus = 800e-6
+    dis_StoD = 2
+    dx = lambda0 * dis_StoD / shape / dx_dec
+
+    # test single probe modes
+    probe = tike.ptycho.fresnel.single_probe(
+        shape,
+        lambda0,
+        dx,
+        dis_defocus,
+        zone_plate_params=dict(
+            radius=150e-6 / 2,
+            outmost=50e-9,
+            beamstop=60e-6,
+        ),
+    )
+
+    plt.imsave(
+        f'{out_dir}/probe-amplitude.png',
+        np.abs(probe[0, 0, 0]),
+    )
+    plt.imsave(
+        f'{out_dir}/probe-phase.png',
+        np.angle(probe[0, 0, 0]),
+        vmin=-np.pi,
+        vmax=np.pi,
+        cmap=plt.cm.twilight,
+    )
+
+    probe = probe[0, 0, 0]
+
+    with np.load(
+            f"{os.environ['HOME']}/Documents/fluffy-umbrella/test/Imagenet64/train_data_batch_1.npz"
+    ) as f:
+        patches = f['data'].reshape(-1, 3, 64, 64)[:, 0] / 255.0 - 0.5
+        print(patches.shape, patches.dtype, patches.min(), patches.max())
+
+    angle = np.pi * patches
+
+    wavefront = (probe * 1.0 * np.exp(1j * angle)).astype(np.complex64)
+
+    diffraction = np.fft.ifftshift(
+        np.square(np.abs(np.fft.fft2(wavefront))),
+        axes=(-2, -1),
+    ).astype(np.float32)
+    print(diffraction.dtype, diffraction.shape)
+
+    samples = np.linspace(0, num_scan, num=5, dtype=int)
+
+    plt.imsave(
+        f'{out_dir}/diffraction-imagenet.png',
+        np.concatenate(diffraction[samples], axis=-1),
+        cmap=plt.cm.gray,
+    )
+    plt.imsave(
+        f'{out_dir}/angle-imagenet.png',
+        np.concatenate(angle[samples], axis=-1),
+        vmin=-np.pi,
+        vmax=+np.pi,
+        cmap=plt.cm.twilight,
+    )
+
+    print(f'Training params = {np.prod(diffraction.shape)}')
+
+    np.savez(
+        f'{out_dir}/simulated-imagenet.npz',
+        reciprocal=diffraction,
+        real=angle,
+    )
+
+
+if __name__ == '__main__':
+    test_construct_simulated_training_set()
